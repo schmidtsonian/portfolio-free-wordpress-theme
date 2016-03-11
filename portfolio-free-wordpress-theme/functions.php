048304a1122a6683501890daa36b4a4eacf10b52
Esteban <jramirezschmidt@gmail.com>
2016-03-10 23:14:33 -0600

    Remove unused pages

--- portfolio-free-wordpress-theme/functions.php
+++ portfolio-free-wordpress-theme/functions.php
@@ -16,31 +16,13 @@ function add_custom_page( $pageName, $pageTitle ) {
         }
     }
 
-// add_custom_page( 'home', 'Home' );
 add_custom_page( 'about', 'About' );
-// add_custom_page( 'blog', 'Blog' );
 
 function add_custom_menu_page() {
     $page = get_page_by_path( 'about' );
     add_menu_page(
         __( 'About' ),
         __( 'About' ),
-        'manage_options',
-        'post.php?post=' . $page->ID . '&action=edit'
-    );
-    
-    $page = get_page_by_path( 'blog' );
-    add_menu_page(
-        __( 'Blog' ),
-        __( 'Blog' ),
-        'manage_options',
-        'post.php?post=' . $page->ID . '&action=edit'
-    );
-    
-    $page = get_page_by_path( 'home' );
-    add_menu_page(
-        __( 'Home' ),
-        __( 'Home' ),
         'manage_options',
         'post.php?post=' . $page->ID . '&action=edit'
     );
